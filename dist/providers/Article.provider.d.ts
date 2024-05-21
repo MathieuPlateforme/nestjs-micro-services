630d21a996b6c5ea8a25daa1781f172c7ae464c6
MathieuPlateforme <107928881+MathieuPlateforme@users.noreply.github.com>
2024-05-21 09:49:28 +0200

    Delete dist directory

--- dist/providers/Article.provider.d.ts
+++
@@ -1,7 +0,0 @@
-import { DataSource } from 'typeorm';
-import { Article } from 'src/entities/Article.entity';
-export declare const articleProviders: {
-    provide: string;
-    useFactory: (dataSource: DataSource) => import("typeorm").Repository<Article>;
-    inject: string[];
-}[];
