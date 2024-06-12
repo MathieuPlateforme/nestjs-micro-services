286e9945198e0caee34a2ef8f27f1a4585a5955f
MathieuPlateforme <mathieu.ruiz@laplateforme.io>
2024-06-12 11:08:14 +0200

    update gateway

--- gateway-service/src/app.controller.ts
+++ gateway-service/src/app.controller.ts
@@ -1,42 +1,19 @@
-import { Controller, Post, Body, Get, Param, Inject, HttpStatus, HttpException } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { Controller, Post, Body, Get, Param, HttpStatus, HttpException } from '@nestjs/common';
 import { catchError, firstValueFrom } from 'rxjs';
 import { CreateOrderDto } from './create-order.dto';
+import { AppService } from './app.service';
 
 @Controller('orders')
 export class ApiGatewayController {
-    constructor(@Inject('ORDERS_SERVICE') private readonly commandService: ClientProxy) {}
+    constructor(private readonly appService: AppService) {}
 
     @Post('order')
     async createOrder(@Body() createOrderDto: CreateOrderDto) {
-        try {
-            const response = await firstValueFrom(
-                this.commandService.send('create_order', createOrderDto).pipe(
-                    catchError(err => {
-                        throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
-                    })
-                )
-            );
-            console.log('Order Created Event received:', response);
-            return response;
-        } catch (err) {
-            throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return this.appService.createOrder(createOrderDto);;
     }
 
     @Get(':id')
     async getOrder(@Param('id') id: string) {
-        try {
-            const response = await firstValueFrom(
-                this.commandService.send('get_order', id).pipe(
-                    catchError(err => {
-                        throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
-                    })
-                )
-            );
-            return response;
-        } catch (err) {
-            throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return this.appService.getOrder(id);;
     }
 }
--- gateway-service/src/app.service.ts
+++ gateway-service/src/app.service.ts
@@ -1,8 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, HttpStatus, HttpException } from '@nestjs/common';
+import { catchError, firstValueFrom } from 'rxjs';
+import { ClientProxy } from '@nestjs/microservices';
+import { CreateOrderDto } from './create-order.dto';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@Inject('ORDERS_SERVICE') private readonly commandService: ClientProxy) {}
+
+
+  async createOrder(createOrderDto: CreateOrderDto) {
+    let messageData = {};
+    try {
+      const response = await firstValueFrom(
+        this.commandService.send('create_order', createOrderDto).pipe(
+            catchError(err => {
+                throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
+            })
+        )
+      );
+      messageData = {... {"order": response}};
+    } catch (err) {
+        throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+    return messageData;
+  }
+
+  async getOrder(id: string) {
+    try {
+      const response = await firstValueFrom(
+        this.commandService.send('get_order', id).pipe(
+            catchError(err => {
+                throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
+            })
+        )
+    );
+    return response;
+    } catch (err) {
+        throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
